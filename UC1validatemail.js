7e5069ebfce106bdfa3b976bca811ee94fdd2060
Ankit0427 <153299876+Ankit0427@users.noreply.github.com>
2025-03-06 11:04:20 +0530

    Update UC1validatemail.js

--- UC1validatemail.js
+++ UC1validatemail.js
@@ -5,17 +5,17 @@ let validateEmail=(email)=> {
 
 // Test Cases
 console.log(validateEmail("abc@bridgelabz.co"));     
-console.log(validateEmail("abc.xyz@bridgelabz.co"));    // ✅ true
-console.log(validateEmail("abc_xyz@bridgelabz.co"));    // ✅ true,
-console.log(validateEmail("abc-xyz@bridgelabz.co"));    // ✅ true
-console.log(validateEmail("abc+xyz@bridgelabz.co.in")); // ✅ true
+console.log(validateEmail("abc.xyz@bridgelabz.co"));    
+console.log(validateEmail("abc_xyz@bridgelabz.co"));    
+console.log(validateEmail("abc-xyz@bridgelabz.co"));    
+console.log(validateEmail("abc+xyz@bridgelabz.co.in")); 
 
-console.log(validateEmail("abc@bridgelabz.co.in"));     // ✅ true
-console.log(validateEmail("abc.xyz@bridgelabz.co.in")); // ✅ true
-console.log(validateEmail("abc-xyz@bridgelabz.co.in")); // ✅ true
-console.log(validateEmail("abc+xyz@bridgelabz.co.in")); // ✅ true
+console.log(validateEmail("abc@bridgelabz.co.in"));     
+console.log(validateEmail("abc.xyz@bridgelabz.co.in")); 
+console.log(validateEmail("abc-xyz@bridgelabz.co.in")); 
+console.log(validateEmail("abc+xyz@bridgelabz.co.in")); 
 
-console.log(validateEmail("abc!xyz@bridgelabz.co.in")); // ❌ false (Invalid character `!`)
-console.log(validateEmail("abc@xyz.co.in"));           // ❌ false (Must be `@bridgelabz`)
-console.log(validateEmail("abc@bridgelabz.com"));      // ❌ false (Must contain `.co`)
-console.log(validateEmail("xyz@bridgelabz.co.in"));    // ❌ false (Must start with `abc`)
+console.log(validateEmail("abc!xyz@bridgelabz.co.in")); 
+console.log(validateEmail("abc@xyz.co.in"));          
+console.log(validateEmail("abc@bridgelabz.com"));      
+console.log(validateEmail("xyz@bridgelabz.co.in"));    
